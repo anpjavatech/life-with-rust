eb9a24762860c7de8fed6b752466fd0005aeb7f7
Anoop Kuttikattu Sukumaran Nair <anoop.ks007@gmail.com>
2025-05-23 23:29:41 +0200

    structs with debug format and methods

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,12 +1,19 @@
-fn main() {
-    struct User {
-        name: String,
-        age: u32,
-        username: String,
-        email: String,
-        active: bool,
-    }
+#[derive(Debug)]
+struct User {
+    name: String,
+    age: u32,
+    username: String,
+    email: String,
+    active: bool,
+}
 
+impl User {
+    fn check_if_adult(&self) -> bool {
+        self.age >= 18
+    }
+}
+
+fn main() {
     let user1 = User {
         name: String::from("Anoop"),
         age: 37,
@@ -20,4 +27,10 @@ fn main() {
     println!("username of the created user : {0}", user1.username);
     println!("email of the created user : {0}", user1.email);
     println!("active of the created user : {0}", user1.active);
+
+    dbg!(&user1);
+
+    // check if the user is an adult or not
+    let is_adult = dbg!(user1.check_if_adult());
+    println!("Is the user an Adult : {is_adult}");
 }
